106187dd627e750da5b99e263e5401cd3eb553c1
Bennett <potgieterbt@gmail.com>
2021-08-05 21:06:24 +0100

    env secret key

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import TemplateView
 
-from . import views
+from .views import *
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', Index.as_view(), name='index'),
+    path('profile', Profile.as_view(), name='index'),
+    path('about', About.as_view(), name='index'),
+    path('menu', Menu.as_view(), name='index'),
+    path('coffee', Coffee.as_view(), name='index'),
+    path('account/regular', Regular.as_view(), name='index'),
+    path('order', Order.as_view(), name='index'),
+    path('product/<product>', Products.as_view(), name='product'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,77 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.base import View
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Hey")
+
+class Index(View):
+    def __init__(self):
+        self.template_name = "index.html"
+
+    def get(self, request):
+        context = {}
+        return render(request, self.template_name, context)
+
+
+class Products(View):
+    def __init__(self):
+        self.template_name = "products.html"
+
+    def get(self, request):
+        context = {}
+        return render(request, self.template_name, context)
+
+
+class About(View):
+    def __init__(self):
+        self.template_name = "about.html"
+
+    def get(self, request):
+        context = {}
+        return render(request, self.template_name, context)
+
+
+class Menu(View):
+    def __init__(self):
+        self.template_name = "menu.html"
+
+    def get(self, request):
+        context = {}
+        return render(request, self.template_name, context)
+
+
+class Coffee(View):
+    def __init__(self):
+        self.template_name = "coffee.html"
+
+    def get(self, request):
+        context = {}
+        return render(request, self.template_name, context)
+
+
+class Regular(View):
+    def __init__(self):
+        self.template_name = "regular.html"
+
+    def get(self, request):
+        context = {}
+        return render(request, self.template_name, context)
+
+
+class Order(View):
+    def __init__(self):
+        self.template_name = "order.html"
+
+    def get(self, request):
+        context = {}
+        return render(request, self.template_name, context)
+
+
+class Profile(View):
+    def __init__(self):
+        self.template_name = "profile.html"
+
+    def get(self, request):
+        context = {}
+        return render(request, self.template_name, context)
